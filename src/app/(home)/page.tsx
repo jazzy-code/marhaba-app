4c9003c3fec856041732a218eabf7adc56fc2838
Javier Torres <javiertorresm2000@gmail.com>
2026-04-28 18:22:45 -0600

    test: remove new componente liveitthisweek

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -189,7 +189,7 @@ export default function HomePage() {
       </section>
 
       {/* Weekly Features Section */}
-      <LiveItThisWeek />
+      {/* <LiveItThisWeek /> */}
 
       {/* Stats Section */}
       <section className="border-y border-deep-brown/10 bg-deep-brown py-12">
